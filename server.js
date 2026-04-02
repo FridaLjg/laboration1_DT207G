239acdf7de4edd4eb02f76b87b2170d31d7cb47b
Frida Ljungberg <frlj2501@student.miun.se>
2026-04-02 14:46:11 +0200

    lagt till index.ejs, add.ejs och about.ejs. Har också lagt till så de kan köras.

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const mysql = require("mysql");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public")); 
+app.set("view engine", "ejs");
 
 // Anslutningsinställningar
 const connection = mysql.createConnection({
@@ -17,3 +22,20 @@ connection.connect((err => {
     console.log("Connected to MySQL!");
 }));
 
+// Routing
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+// Start applikation
+app.listen(port, () => {
+    console.log("Servern har startat på port:" + port);
+});
